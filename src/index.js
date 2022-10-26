a882149d66a98b33b4639b9813ff8d4104c73d3d
imporvKing4 <philipalexander572@gmail.com>
2022-10-26 16:08:36 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Coaches from './Coaches/coaches';
+import Blog from './Blog/blog';
+import Products from './products/product';
+import ProductDetails from './products/productDetail';
+import GetStarted from './getStarted/getStarted';
+import Error  from './error/error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' index exact element={<App /> } />
+        <Route path='/Products' exact element={<Products />} />
+        <Route path='/product/:productId' exact element={<ProductDetails />} />
+        <Route path='/Coaches' exact element={<Coaches />} />
+        <Route path='/Blog' exact element={<Blog />} />
+        <Route path='/pricings' exact element={<GetStarted />} />
+       <Route path='*' element={<Error /> } /> 
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
